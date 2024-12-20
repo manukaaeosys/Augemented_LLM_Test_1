500aeeff5bc266ff6643291783d055227919fd6e
manukakeg <manukakeg@gmail.com>
2024-12-20 13:42:09 +0530

    first commit

--- app.py
+++ app.py
@@ -19,6 +19,7 @@ def get_pdf_text(pdf_docs):
         for page in pdf_reader.pages:
             text += page.extract_text()
     return text
+
 '''
 def get_text_chunks(text):
     text_splitter = CharacterTextSplitter(
